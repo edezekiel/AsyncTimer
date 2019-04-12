eb06481e293c85a736a4fe3482d8ea619fbbb20c
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-04-12 14:21:44 -0400

    first commit

---
+++ async.js
@@ -0,0 +1,24 @@
+let button = document.querySelector("button")
+
+let secondsInput = document.querySelector("input")
+
+button.addEventListener('click',  () => asyncCall(parseInt(secondsInput.value)))
+
+let remaining = document.querySelector("#remaining")
+
+function resolveAfter1Second() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve('resolved');
+    }, 1000);
+  });
+}
+
+async function asyncCall(x) {
+   remaining.innerHTML = x;
+  while (x > 0) {
+    let result = await resolveAfter1Second();
+    x--;
+    remaining.innerHTML = x;
+  }
+}
